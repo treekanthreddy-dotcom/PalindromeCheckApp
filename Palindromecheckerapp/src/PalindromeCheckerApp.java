fc5dc974bb9274bb0134f34072f8bbc413bd38c3
treekanthreddy-dotcom <treekanthreddy@gmail.com>
2026-03-23 22:09:29 +0530

    Update PalindromeCheckerApp.java

--- Palindromecheckerapp/src/PalindromeCheckerApp.java
+++ Palindromecheckerapp/src/PalindromeCheckerApp.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class PalindromeCheckerApp {
 
-    // Recursive reverse function
     public static String reverseString(String str) {
         if (str.isEmpty()) {
             return str;
@@ -17,13 +16,10 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Normalize input
         String cleaned = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        // Reverse using recursion
         String reversed = reverseString(cleaned);
 
-        // Compare
         if (cleaned.equals(reversed)) {
             System.out.println("Result: It is a Palindrome.");
         } else {
