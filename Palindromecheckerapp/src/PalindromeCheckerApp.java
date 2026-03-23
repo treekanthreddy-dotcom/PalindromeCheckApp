b85ff1e8e6838a9344bb13f3fe8d767825516178
treekanthreddy-dotcom <treekanthreddy@gmail.com>
2026-03-23 22:09:55 +0530

    Update PalindromeCheckerApp.java

--- Palindromecheckerapp/src/PalindromeCheckerApp.java
+++ Palindromecheckerapp/src/PalindromeCheckerApp.java
@@ -10,16 +10,13 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Normalize input
         String cleaned = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        // Reverse using Streams
         String reversed = IntStream.range(0, cleaned.length())
                 .mapToObj(i -> cleaned.charAt(cleaned.length() - 1 - i))
                 .map(String::valueOf)
                 .collect(Collectors.joining());
 
-        // Compare
         if (cleaned.equals(reversed)) {
             System.out.println("Result: It is a Palindrome.");
         } else {
