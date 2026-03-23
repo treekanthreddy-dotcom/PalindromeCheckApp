ac07b3271ddb90d27e8abc29fc2508f9bda6a244
treekanthreddy-dotcom <treekanthreddy@gmail.com>
2026-03-23 22:07:18 +0530

    Update PalindromeCheckerApp.java

--- Palindromecheckerapp/src/PalindromeCheckerApp.java
+++ Palindromecheckerapp/src/PalindromeCheckerApp.java
@@ -9,13 +9,11 @@ public class UseCase7PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Normalize input (remove spaces + convert to lowercase)
         input = input.replaceAll("\\s+", "").toLowerCase();
 
         Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
-        // Add characters to Queue and Stack
         for (int i = 0; i < input.length(); i++) {
             char ch = input.charAt(i);
             queue.add(ch);
@@ -24,7 +22,6 @@ public class UseCase7PalindromeCheckerApp {
 
         boolean isPalindrome = true;
 
-        // Compare queue and stack
         while (!queue.isEmpty()) {
             if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
@@ -32,7 +29,6 @@ public class UseCase7PalindromeCheckerApp {
             }
         }
 
-        // Output result
         if (isPalindrome) {
             System.out.println("Result: It is a Palindrome.");
         } else {
